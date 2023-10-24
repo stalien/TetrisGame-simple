98830aa252163693f55562606f3e257328d71d65
stalien <stalien@yandex.ru>
2023-10-24 14:24:29 +0300

    create empty window

--- src/GamePanel.java
+++ src/GamePanel.java
@@ -1,2 +1,15 @@
-package PACKAGE_NAME;public class GamePanel {
+import javax.swing.*;
+import java.awt.*;
+
+public class GamePanel extends JPanel {
+    public static final int WIDTH = 1280;
+    public static final int HEIGHT = 720;
+
+    public GamePanel() {
+
+        this.setPreferredSize(new Dimension(WIDTH, HEIGHT));
+        this.setBackground(Color.BLACK);
+        this.setLayout(null);
+
+    }
 }
