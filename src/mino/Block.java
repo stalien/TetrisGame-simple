ded23edfd7220fafb7e0faeff745b2beea2098c7
stalien <stalien@yandex.ru>
2023-10-24 15:58:46 +0300

    create class for mino blocks

--- src/mino/Block.java
+++ src/mino/Block.java
@@ -1,2 +1,21 @@
-package mino;public class Block {
+package mino;
+
+import java.awt.*;
+
+public class Block extends Rectangle {
+
+    public int x;
+    public int y;
+    public static final int SIZE = 30;
+    public Color c;
+
+    public Block(Color c) {
+        this.c = c;
+    }
+
+    public void draw(Graphics2D g2) {
+        g2.setColor(c);
+        g2.fillRect(x, y, SIZE, SIZE);
+    }
+
 }
