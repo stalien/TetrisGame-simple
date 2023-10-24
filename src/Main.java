02618659808e4a67a90dab7364f2d5bd9870f75b
stalien <stalien@yandex.ru>
2023-10-24 14:59:57 +0300

    draw play area frame

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@ import javax.swing.*;
 
 public class Main {
     public static void main(String[] args) {
-        
+
         JFrame window = new JFrame("Simple tetris");
         window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         window.setResizable(false);
