9d9a429838061cc3ddfd9a9b611dff4c51cb4b4e
stalien <stalien@yandex.ru>
2023-10-25 13:44:53 +0300

    add rotation

--- src/mino/Mino.java
+++ src/mino/Mino.java
@@ -5,11 +5,12 @@ import java.awt.*;
 import main.KeyHandler;
 import main.PlayManager;
 
-public class Mino {
+public abstract class Mino {
 
     public Block[] b = new Block[4];
     public Block[] tempB = new Block[4];
     int autoDropCounter = 0;
+    public int direction = 1;
 
     public void create(Color c) {
 
@@ -20,18 +21,42 @@ public class Mino {
 
     }
 
-    public void setXY(int x, int y) {
-
-    }
+    public abstract void setXY(int x, int y);
 
     public void updateXY(int direction) {
 
+        this.direction = direction;
+
+        System.arraycopy(tempB, 0, b, 0, b.length);
+
     }
+
+    public abstract void setDirection1();
+    public abstract void setDirection2();
+    public abstract void setDirection3();
+    public abstract void setDirection4();
 
     public void update() {
 
         // move controls
         if (KeyHandler.upPressed) {
+
+            switch (direction) {
+                case 1 :
+                    setDirection2();
+                    break;
+                case 2 :
+                    setDirection3();
+                    break;
+                case 3 :
+                    setDirection4();
+                    break;
+                case 4 :
+                    setDirection1();
+                    break;
+            }
+
+            KeyHandler.upPressed = false;
 
         }
         if (KeyHandler.downPressed) {
--- src/mino/Mino_L1.java
+++ src/mino/Mino_L1.java
@@ -24,4 +24,77 @@ public class Mino_L1 extends Mino {
         b[3].y = b[0].y + Block.SIZE;
 
     }
+
+    @Override
+    public void setDirection1() {
+        //  o       b[1]
+        //  o       b[0]
+        //  o o     b[2]b[3]
+        tempB[0].x = b[0].x;
+        tempB[0].y = b[0].y;
+        tempB[1].x = b[0].x;
+        tempB[1].y = b[0].y - Block.SIZE;
+        tempB[2].x = b[0].x;
+        tempB[2].y = b[0].y + Block.SIZE;
+        tempB[3].x = b[0].x + Block.SIZE;
+        tempB[3].y = b[0].y + Block.SIZE;
+
+        updateXY(1);
+    }
+
+    @Override
+    public void setDirection2() {
+        //
+        //  o o o   b[2]b[0]b[1]
+        //  o       b[3]
+        //
+        tempB[0].x = b[0].x;
+        tempB[0].y = b[0].y;
+        tempB[1].x = b[0].x + Block.SIZE;
+        tempB[1].y = b[0].y;
+        tempB[2].x = b[0].x - Block.SIZE;
+        tempB[2].y = b[0].y;
+        tempB[3].x = b[0].x - Block.SIZE;
+        tempB[3].y = b[0].y + Block.SIZE;
+
+        updateXY(2);
+
+    }
+
+    @Override
+    public void setDirection3() {
+        //  o o b[3]b[2]
+        //    o     b[0]
+        //    o     b[1]
+        tempB[0].x = b[0].x;
+        tempB[0].y = b[0].y;
+        tempB[1].x = b[0].x;
+        tempB[1].y = b[0].y + Block.SIZE;
+        tempB[2].x = b[0].x;
+        tempB[2].y = b[0].y - Block.SIZE;
+        tempB[3].x = b[0].x - Block.SIZE;
+        tempB[3].y = b[0].y - Block.SIZE;
+
+        updateXY(3);
+
+    }
+
+    @Override
+    public void setDirection4() {
+        //
+        //      o           b[3]
+        //  o o o   b[1]b[0]b[2]
+        //
+        tempB[0].x = b[0].x;
+        tempB[0].y = b[0].y;
+        tempB[1].x = b[0].x - Block.SIZE;
+        tempB[1].y = b[0].y;
+        tempB[2].x = b[0].x + Block.SIZE;
+        tempB[2].y = b[0].y;
+        tempB[3].x = b[0].x + Block.SIZE;
+        tempB[3].y = b[0].y - Block.SIZE;
+
+        updateXY(4);
+
+    }
 }
